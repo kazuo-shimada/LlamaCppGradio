2c24fe1622314a79c64903a7b29487cb03895dfe
kazuo-shimada <seankshimada@gmail.com>
2025-12-10 00:20:51 -0800

    fixing reference error in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gradio as gr
 import src.p3g  # if under src/, ensure `pip install -e .` or PYTHONPATH includes src
 
 def main():
-    demo = p3g.build_ui()
+    demo = src.p3g.build_ui()
     demo.queue(max_size=32).launch(server_port=7860, server_name="0.0.0.0")
 
 if __name__ == "__main__":
