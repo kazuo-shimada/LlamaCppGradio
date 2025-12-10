1a49e46b96b529a16e4aac66edc34c8cd31617d6
kazuo-shimada <seankshimada@gmail.com>
2025-12-09 17:51:56 -0800

    Implement fake llama_cpp module for tests
    Added fake handlers and chat formats for testing purposes.

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,13 +1,39 @@
-# tests/test_sanity.py
+import types
+import sys
 
-def test_math_works():
-    assert 1 + 1 == 2
+# ----- Fake llama_cpp.llama_chat_format -----
+lcf = types.ModuleType("llama_cpp.llama_chat_format")
 
+class _DummyHandler:
+    def __init__(self, clip_model_path=None, mmproj=None, **kwargs):
+        self.clip_model_path = clip_model_path or mmproj
 
-def test_true_is_true():
-    assert True
+Llava15ChatHandler = _DummyHandler
+Llava16ChatHandler = _DummyHandler
+MoondreamChatHandler = _DummyHandler
+NanollavaChatHandler = _DummyHandler
+Llama3VisionAlphaChatHandler = _DummyHandler
+MiniCPMv26ChatHandler = _DummyHandler
+Qwen25VLChatHandler = _DummyHandler
 
+lcf.CHAT_FORMATS = {"llama-2": object(), "chatml": object()}
+lcf.Llava15ChatHandler = Llava15ChatHandler
+lcf.Llava16ChatHandler = Llava16ChatHandler
+lcf.MoondreamChatHandler = MoondreamChatHandler
+lcf.NanollavaChatHandler = NanollavaChatHandler
+lcf.Llama3VisionAlphaChatHandler = Llama3VisionAlphaChatHandler
+lcf.MiniCPMv26ChatHandler = MiniCPMv26ChatHandler
+lcf.Qwen25VLChatHandler = Qwen25VLChatHandler
 
-def test_string_contains():
-    s = "edgerunner"
-    assert "edge" in s
+# ----- Fake llama_cpp root with Llama -----
+llama_cpp = types.ModuleType("llama_cpp")
+
+class Llama:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+llama_cpp.Llama = Llama
+llama_cpp.llama_chat_format = lcf
+
+sys.modules["llama_cpp"] = llama_cpp
+sys.modules["llama_cpp.llama_chat_format"] = lcf
